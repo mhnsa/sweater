e88e49f3f6ac31492f6ad71d071bc2a3f400be08
Marsel <h096hk@gmail.com>
2021-12-05 20:59:56 +0300

    Add Freemarker (lesson 5)

--- src/main/java/com/example/sweater/controller/MainController.java
+++ src/main/java/com/example/sweater/controller/MainController.java
@@ -8,6 +8,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -26,10 +27,18 @@ public class MainController {
     }
 
     @GetMapping("/main")
-    public String main(Map<String, Object> model) {
+    public String main(@RequestParam(required = false, defaultValue = " ") String filter,
+                       Model model) {
         Iterable<Message> messages = messageRepository.findAll();
 
-        model.put("messages", messages);
+        if (StringUtils.isNotBlank(filter)) {
+            messages = messageRepository.findByTag(filter);
+        } else {
+            messages = messageRepository.findAll();
+        }
+        model.addAttribute("messages", messages);
+        model.addAttribute("filter", filter);
+
         return "main";
     }
 
@@ -38,32 +47,16 @@ public class MainController {
             @AuthenticationPrincipal User user,
             @RequestParam String text,
             @RequestParam String tag,
-            Map<String, Object> model) {
+            Model model) {
         var message = new Message(text, tag, user);
 
         messageRepository.save(message);
 
-        Iterable<Message> messages = messageRepository.findAll();
+//        Iterable<Message> messages = messageRepository.findAll();
+//
+//        model.addAttribute("messages", messages);
 
-        model.put("messages", messages);
-
-        return "main";
+        return "redirect:/main";
     }
-
-    @PostMapping("filter")
-    public String filter(@RequestParam String filter,
-                         Map<String, Object> model) {
-        Iterable<Message> messages;
-
-        if (StringUtils.isNotBlank(filter)) {
-            messages = messageRepository.findByTag(filter);
-        } else {
-            messages = messageRepository.findAll();
-        }
-        model.put("messages", messages);
-
-        return "main";
-    }
-
 
 }
